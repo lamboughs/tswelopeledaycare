4b2519abc6072d8b05a75dfd3ba41d79bd07a421
Lindo Mlambo <39390302+lamboughs@users.noreply.github.com>
2018-10-04 13:21:28 +0200

    Update mail.php

--- mail.php
+++ mail.php
@@ -2,7 +2,8 @@
 	require_once 'phpMailer/PHPMailer.php';
 	require_once 'phpMailer/SMTP.php';
 	require_once 'phpMailer/Exception.php';
-	if(isset($_POST)){
+
+	if(isset($_POST['send_mail'])){
 		$email = $_POST['email'];
 		$message = $_POST['message'];
 		$name = $_POST['names'];
@@ -22,9 +23,14 @@
 	//	$mail->AddAddress('orifha@mbedzi.co.za');
 		$mail->Send();
 		if(!$mail->Send()) {
-        	echo "Mailer Error: " . $mail->ErrorInfo;
-     	} else {
-        echo "Message has been sent";
-     }
- }
+			$mail_status = "danger"; // for bootstrap notification class
+        		$mail_msg =  "Mailer Error: " . $mail->ErrorInfo;
+		} else {
+			$mail_status = "success"; // for bootstrap notification class
+			$mail_msg =  "Message has been sent";
+		}
+		$results = ["status"=>$mail_status, "message"=>$mail_msg];
+		$results = json.encode($results);
+		print_r($results);
+ 	}
 ?>
